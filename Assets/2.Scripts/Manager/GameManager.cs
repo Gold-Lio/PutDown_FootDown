273d9285b43a467d5eb43506bcf182972fd0e644
Kwonkuhwan <kkh3486@naver.com>
2022-09-01 23:29:26 +0900

    2022.09.01 - Inventory Ready
    - 인벤토리 밑 준비
    - 문제가 있는데 drop할때 인풋액션이 버전문제로 안된다 이거 생각해야한다.
    - 다른 자잘한 문제가 있긴하지만 일단 호환부터 맞추자

--- Assets/2.Scripts/Manager/GameManager.cs
+++ Assets/2.Scripts/Manager/GameManager.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Item 관련 --------------------------------------------------------------------------------
+    private ItemDataManager itemData;
+    public ItemDataManager ItemData
+    {
+        get => itemData;
+    }
+    // ------------------------------------------------------------------------------------------
 
-   
-    
+    // Inven 관련 --------------------------------------------------------------------------------
+    private InventoryUI inventoryUI;
+    public InventoryUI InvenUI => inventoryUI;
+    // ------------------------------------------------------------------------------------------
 
+    // 게임 매니저의 인스턴스 생성
+    static GameManager instance = null;
+
+    // 프로퍼티로 게임매니저 인스턴스 접근 (보안 이슈)
+    public static GameManager Inst
+    {
+        get => instance;
+    }
+
+    private void Awake()
+    {
+        // 인스턴스가 null이면 생성
+        if (instance == null)
+        {
+            instance = this;
+            instance.Initialize();
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else // 아니라면 현재 인스턴스가 게임매니저인지 확인하고 아니라면 삭제
+        {
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 게임매니저 초기화 (게임매니저가 재생성될때 Awake가 실행될때 컴포지트를 중복으로 불러올수 있어서 생성
+    /// </summary>
+    private void Initialize()
+    {
+    }
 }
